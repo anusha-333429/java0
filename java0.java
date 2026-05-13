5b558bc00ef662a12a8234d4e55487a43d858ea5
Anu <anushachitra429@gmail.com>
2026-05-13 08:54:56 +0530

    Update java0.java

--- java0.java
+++ java0.java
@@ -1,6 +1,6 @@
-
+//syntax for printing something on the screen
 class java0{
-    public static void main(String[] args){
-         System.out.println("Hello World!");
+    public static void main(String[] args){      
+         System.out.println("Hello World!");    //text which is going to print on screen should be with in ""
     }
 }
